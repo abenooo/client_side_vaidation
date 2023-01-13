193f377201d6386980c0466ca337b60436dc0ee3
Infinity Dev1 <infinitydev1@InfinityVDI007.cbe.com.et>
2023-01-13 17:09:00 +0300

    Create reusable input componets

---
+++ src/componets/FormInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './formInput.css'
+const FormInput = () => {
+  return (
+    <div className='formInput'>
+        <label>Username</label>
+        <input />
+    </div>
+  )
+}
+
+export default FormInput
